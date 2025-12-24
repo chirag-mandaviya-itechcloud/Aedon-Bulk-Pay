39b5e41e02964b656c33575199fef9b430e5cd42
chirag-mandaviya-itechcloud <chirag.mandaviya@itechcloudsolution.com>
2025-12-24 14:08:50 +0530

    make component dynamic for both sales and purchase invoice

--- force-app/main/default/lwc/bulkPaymentFeature/bulkPaymentFeature.js
+++ force-app/main/default/lwc/bulkPaymentFeature/bulkPaymentFeature.js
@@ -1,8 +1,10 @@
 import { LightningElement, api, track } from 'lwc';
 import LightningAlert from 'lightning/alert';
 import getPurchaseInvoiceDetails from '@salesforce/apex/BulkPaymentFeatureController.getPurchaseInvoiceDetails';
+import getSalesInvoiceDetails from '@salesforce/apex/BulkPaymentFeatureController.getSalesInvoiceDetails';
 import getBankAccounts from '@salesforce/apex/BulkPaymentFeatureController.getBankAccounts';
 import processBulkPayment from '@salesforce/apex/BulkPaymentFeatureController.processBulkPayment';
+import getInvoiceType from '@salesforce/apex/BulkPaymentFeatureController.getInvoiceType';
 
 export default class BulkPaymentFeature extends LightningElement {
     @api recordIds;
@@ -15,8 +17,9 @@ export default class BulkPaymentFeature extends LightningElement {
     @track exchangeRate = '';
     @track paymentReference = '';
     @track bankAccountOptions = [];
-
-    isLoading = false;
+    @track invoiceType = '';
+    @track invoiceInfo = {};
+    @track isLoading = false;
 
     // Define columns for lightning-datatable
     columns = [
@@ -28,13 +31,13 @@ export default class BulkPaymentFeature extends LightningElement {
         },
         {
             label: 'Account',
-            fieldName: 'accountName',
+            fieldName: this.accountNameFieldName,
             type: 'text',
             sortable: true
         },
         {
             label: 'Invoice Date',
-            fieldName: 'natdev24__Invoice_Date__c',
+            fieldName: this.invoiceDateFieldName,
             type: 'date',
             sortable: true,
             typeAttributes: {
@@ -45,42 +48,138 @@ export default class BulkPaymentFeature extends LightningElement {
         },
         {
             label: 'Customer Reference',
-            fieldName: 'natdev24__Customer_Reference__c',
+            fieldName: this.customerReferenceFieldName,
             type: 'text',
             sortable: true
         },
         {
             label: 'Gross Amount',
-            fieldName: 'natdev24__Gross_Amount__c',
+            fieldName: this.grossAmountFieldName,
             type: 'number'
         },
         {
             label: 'Outstanding Amount',
-            fieldName: 'natdev24__Balance_Outstanding__c',
+            fieldName: this.outstandingAmountFieldName,
             type: 'number'
         },
 
     ];
 
-    connectedCallback() {
-        // console.log('Record Ids :', this.recordIds);
-        this.validateAndLoadData();
-        this.loadBankAccounts();
-        if (this.recordIds) {
-            const recordIdArray = this.recordIds.split(',').map(id => id.trim());
-            this.loadInvoiceDetails(recordIdArray);
+    get accountNameFieldName() {
+        return 'accountName';
+    }
+
+    get invoiceDateFieldName() {
+        switch (this.invoiceType) {
+            case 'Purchase':
+                return 'natdev24__Invoice_Date__c';
+            case 'Sales':
+                return 'natdev24__Invoice_Date__c';
+            default:
+                return 'natdev24__Invoice_Date__c';
         }
     }
 
-    validateAndLoadData() {
+    get customerReferenceFieldName() {
+        switch (this.invoiceType) {
+            case 'Purchase':
+                return 'natdev24__Customer_Reference__c';
+            case 'Sales':
+                return 'natdev24__Customer_Reference__c';
+            default:
+                return 'natdev24__Customer_Reference__c';
+        }
+    }
+
+    get grossAmountFieldName() {
+        switch (this.invoiceType) {
+            case 'Purchase':
+                return 'natdev24__Gross_Amount__c';
+            case 'Sales':
+                return 'natdev24__Gross_Amount__c';
+            default:
+                return 'natdev24__Gross_Amount__c';
+        }
+    }
+
+    get outstandingAmountFieldName() {
+        switch (this.invoiceType) {
+            case 'Purchase':
+                return 'natdev24__Balance_Outstanding__c';
+            case 'Sales':
+                return 'natdev24__Balance_Outstanding__c';
+            default:
+                return 'natdev24__Balance_Outstanding__c';
+        }
+    }
+
+    connectedCallback() {
+        console.log('Record Ids :', this.recordIds);
+        this.checkRecordsIsSelected();
+        this.loadBankAccounts();
+        if (this.recordIds) {
+            this.getObjectTypeAndLoadInvoiceDetails();
+        }
+    }
+
+    checkRecordsIsSelected() {
         if (!this.recordIds) {
             this.showAlertAndReturn('Error', 'Select Invoices with Outstanding Balance', 'error');
         }
     }
 
+    loadBankAccounts() {
+        getBankAccounts()
+            .then(result => {
+                // console.log('Bank Accounts fetched:', result);
+                this.bankAccountOptions = result.map(acc => ({
+                    label: acc.Name,
+                    value: acc.Id
+                }));
+                // console.log('Loaded Bank Accounts:', this.bankAccountOptions);
+            })
+            .catch(error => {
+                console.error('Error loading bank accounts:', error);
+            });
+    }
+
+    getApexControllerMethod() {
+        if (this.invoiceType === 'Purchase') {
+            return getPurchaseInvoiceDetails;
+        } else if (this.invoiceType === 'Sales') {
+            return getSalesInvoiceDetails;
+        }
+
+        return null;
+    }
+
+    getObjectTypeAndLoadInvoiceDetails() {
+        if (this.recordIds && this.recordIds.length > 0) {
+            let recId = this.recordIds.split(',')[0].trim();
+
+            getInvoiceType({ recordId: recId })
+                .then(result => {
+                    this.invoiceInfo = result;
+                    if (result.apiName.includes('Purchase_Invoice')) {
+                        this.invoiceType = 'Purchase';
+                    } else if (result.apiName.includes('Sales_Invoice')) {
+                        this.invoiceType = 'Sales';
+                    }
+                    console.log('Invoice Type:', this.invoiceType);
+                    const recordIdArray = this.recordIds.split(',').map(id => id.trim());
+                    this.loadInvoiceDetails(recordIdArray);
+                })
+                .catch(error => {
+                    console.error('Error fetching invoice type:', error);
+                });
+        }
+    }
+
     loadInvoiceDetails(recordIdArray) {
+        const apexControllerMethod = this.getApexControllerMethod();
         this.isLoading = true;
-        getPurchaseInvoiceDetails({ invoiceIds: recordIdArray })
+
+        apexControllerMethod({ invoiceIds: recordIdArray })
             .then(async result => {
                 // Add accountName field for datatable display
                 this.invoiceRecords = result.map(invoice => ({
@@ -88,7 +187,7 @@ export default class BulkPaymentFeature extends LightningElement {
                     accountName: invoice.natdev24__Account__r?.Name || ''
                 }));
                 this.selectedRows = this.invoiceRecords.map(inv => inv.Id);
-                // console.log('Loaded Invoices:', this.invoiceRecords);
+                console.log('Loaded Invoices:', this.invoiceRecords);
                 this.isLoading = false;
 
                 if (this.invoiceRecords.length === 0) {
@@ -117,31 +216,16 @@ export default class BulkPaymentFeature extends LightningElement {
             });
     }
 
-    loadBankAccounts() {
-        getBankAccounts()
-            .then(result => {
-                // console.log('Bank Accounts fetched:', result);
-                this.bankAccountOptions = result.map(acc => ({
-                    label: acc.Name,
-                    value: acc.Id
-                }));
-                // console.log('Loaded Bank Accounts:', this.bankAccountOptions);
-            })
-            .catch(error => {
-                console.error('Error loading bank accounts:', error);
-            });
-    }
-
     handleRowSelection(event) {
         this.selectedRows = event.detail.selectedRows.map(row => row.Id);
-        // console.log('Selected Rows:', this.selectedRows);
+        console.log('Selected Rows:', this.selectedRows);
     }
 
     handlePaymentFieldChange(event) {
         const field = event.target.dataset.field;
         this[field] = event.target.value;
 
-        // console.log(`Payment field changed: ${field} = ${this[field]}`);
+        console.log(`Payment field changed: ${field} = ${this[field]}`);
     }
 
     get totalAmount() {
@@ -194,7 +278,8 @@ export default class BulkPaymentFeature extends LightningElement {
 
     handleCancel() {
         // console.log('Cancel clicked - forcing navigation');
-        window.location.replace('/lightning/o/natdev24__Purchase_Invoice_Header__c/list?filterName=Recent');
+        url = `/lightning/o/${this.invoiceInfo.apiName}/list?filterName=Recent`
+        window.location.replace(url);
     }
 
     async showAlert(title, message, theme = 'success') {
